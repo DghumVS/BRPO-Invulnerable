ba4392ed7d5c07bbc92f2587ad9c828c5606dfe8
DghumVS <62620001+DghumVS@users.noreply.github.com>
2020-04-21 17:47:45 +0300

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from app import db
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    rights = db.Column(db.String(64))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method='md5')
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    def followed_posts(self):
+        own = Post.query.filter_by(user_id=self.id)
+        return own.order_by(Post.timestamp.desc())
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+from flask_table import Table, Col
+class Results(Table):
+    id = Col('Id', show=False)
+    body = Col('Body')
+    timestamp = Col('Timestamp')
+    user_id = Col('User_id')
